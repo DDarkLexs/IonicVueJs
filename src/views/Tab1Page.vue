e9e10018a1d0d4612a45e455bd59b2272b9f6a5d
antonio lugogo <antoniolexis2001@gmail.com>
2022-02-24 16:11:54 +0100

    no comment

---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,43 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        <ion-searchbar show-cancel-button="always"></ion-searchbar>
+        </ion-toolbar>
+      </ion-header>
+        <ion-card>
+    <ion-item>
+      <ion-icon :icon="pin" slot="start"></ion-icon>
+      <ion-label>Ola mundo</ion-label>
+      <ion-button fill="outline" slot="end">View</ion-button>
+    </ion-item>
+
+    <ion-card-content>
+      This is content, without any paragraph or header tags,
+      within an ion-card-content element.
+    </ion-card-content>
+  </ion-card>
+  
+      <ExploreContainer name="Tab 1 page" />
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default  defineComponent({
+  name: 'Tab1Page',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+});
+</script>
